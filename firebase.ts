c020e7233a7f076a2016c7f42d633e7369d6a0df
Dilsha Pawani <dilshaperera1118@gmail.com>
2025-09-06 09:08:01 +0530

    add firebase.ts

---
+++ firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDZYKZZNONZzTtqB7grknRfaHZopKkCRlI",
+  authDomain: "notezy-f2680.firebaseapp.com",
+  projectId: "notezy-f2680",
+  storageBucket: "notezy-f2680.firebasestorage.app",
+  messagingSenderId: "347940722367",
+  appId: "1:347940722367:web:968f6ac1a9df478376580a",
+  measurementId: "G-39RCY9C8FW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
