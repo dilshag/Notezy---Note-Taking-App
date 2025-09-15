fbb31dfb2327f6d7ea9b772e8d5436bbbefbc4ca
dilshag <dilshaperera1118@gmail.com>
2025-09-15 23:26:11 +0530

    add footer nav

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,38 +1,38 @@
-// import { useRouter, useSegments } from "expo-router"
-// import React from "react"
-// import { Pressable, Text, View } from "react-native"
+import { useRouter, useSegments } from "expo-router"
+import React from "react"
+import { Pressable, Text, View } from "react-native"
 
-// const tabs = [
-//   { lable: "Home", path: "/home" },
-//   { lable: "Project", path: "/project" },
-//   { lable: "User", path: "/user" }
-// ] as const
+const tabs = [
+  { lable: "Home", path: "/home" },
+  { lable: "Project", path: "/project" },
+  { lable: "User", path: "/user" }
+] as const
 
-// const FooterNav = () => {
-//   const router = useRouter()
+const FooterNav = () => {
+  const router = useRouter()
 
-//   const segment = useSegments() // ["project"]
-//   const activeRouter = "/" + (segment[0] || "")
+  const segment = useSegments() // ["project"]
+  const activeRouter = "/" + (segment[0] || "")
 
-//   return (
-//     <View className="flex-row justify-around border-gray-300 py-2 bg-white">
-//       {/* {tabs.map(() => {
-//         return <View></View>
-//       })} */}
-//       {tabs.map((data) => (
-//         <Pressable
-//           // data.path === activeRouter -> this button is active
-//           //   "" + "" -> ` ${can use varibles like any} `
-//           className={`py-1 px-4 rounded-lg ${data?.path === activeRouter ? "bg-blue-600" : ""}`}
-//           onPress={() => {
-//             router.push(data?.path)
-//           }}
-//         >
-//           <Text className="text-2xl">{data?.lable}</Text>
-//         </Pressable>
-//       ))}
-//     </View>
-//   )
-// }
+  return (
+    <View className="flex-row justify-around border-gray-300 py-2 bg-white">
+      {/* {tabs.map(() => {
+        return <View></View>
+      })} */}
+      {tabs.map((data) => (
+        <Pressable
+          // data.path === activeRouter -> this button is active
+          //   "" + "" -> ` ${can use varibles like any} `
+          className={`py-1 px-4 rounded-lg ${data?.path === activeRouter ? "bg-blue-600" : ""}`}
+          onPress={() => {
+            router.push(data?.path)
+          }}
+        >
+          <Text className="text-2xl">{data?.lable}</Text>
+        </Pressable>
+      ))}
+    </View>
+  )
+}
 
-// export default FooterNav
+export default FooterNav
